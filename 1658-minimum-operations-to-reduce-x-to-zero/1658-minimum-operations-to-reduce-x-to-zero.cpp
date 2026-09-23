c0623c9989957cadbf1ccd0c9b0cfe616d374c1a
Ritesh Kumar <igritesh9634@gmail.com>
2026-09-23 22:23:07 +0530

    Time: 0 ms (100%), Space: 102.3 MB (65.24%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int target = total - x;
+
+        // We need to find the longest subarray
+        // whose sum is equal to total - x.
+        if (target < 0) return -1;
+
+        int left = 0, sum = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+
+            while (left <= right && sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+
+        if (maxLen == -1) return -1;
+
+        return nums.size() - maxLen;
+    }
+};
